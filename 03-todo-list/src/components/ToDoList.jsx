e4343bd1c74e8bb98362db47341c6cbaaf9361d2
César Guerra <warderer@gmail.com>
2023-11-29 20:42:08 -0600

    ✨ ToDoList Base

--- 03-todo-list/src/components/ToDoList.jsx
+++ 03-todo-list/src/components/ToDoList.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react'
+
 const ToDoList = () => {
+  // 1. Los estados de React sirven para guardar información que se va a utilizar en el componente. Esta información tiene la particularidad de que se actualiza la vista cada vez que cambia (se ejecuta nuevamente el return).
+  const [inputValue, setInputValue] = useState('')
+
   return (
     <>
       <h1>Lista de Tareas</h1>
+      <input type='text' />
+
+      <button>Agregar</button>
+      <ul>
+        <li>Item 1<button>Eliminar</button></li>
+        <li>Item 2<button>Eliminar</button></li>
+        <li>Item 3<button>Eliminar</button></li>
+      </ul>
     </>
   )
 }
